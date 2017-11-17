cc6265b5f1393072d5af814361ea5261b426c86d
jaswinne <jaswinne@uark.edu>
2017-11-17 00:09:14 -0600

    added CORS so i could access from other port

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(
+	(req, res, next) => {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	;})
 
 const game = require('./controller/gameController');
 const user = require('./controller/userController');

88b8671f8f169c480747bec680ee4b7721393faa
jaswinne <jaswinne@uark.edu>
2017-11-18 17:01:31 -0600

    new actions and reducers for user

--- ttr/src/actions/users.actions.js
+++ ttr/src/actions/users.actions.js
@@ -16,6 +16,52 @@ export const createUserAction = () => {
 		      }).then(res => res.json()).then(
 				data => dispatch({type:CREATE_USER_SUCCESS, data: data}),
 				error => dispatch({type:CREATE_USER_ERROR})
+				//maybe dispatch an action that does some sort of notification on screen?
 			);
 	}
 }
+
+
+export const LOGIN_PENDING = 'LOGIN_PENDING';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_INCORRECT = 'LOGIN_INCORRECT';
+export const LOGIN_ERROR = 'LOGIN_ERROR';
+
+
+
+export const loginAction = (un, pw) => {
+	return (dispatch)=>{
+		dispatch({type:LOGIN_PENDING, data: {un:un, pw:pw}});
+		return fetch('http://localhost:3001/users/login', { 
+		        method: 'POST',
+		        data: {
+		          username: un,
+		          password: pw
+		        }
+		      }).then(res => res.json()).then(
+				data => {
+					//TODO: Check to see if successful login
+					if(true){
+						dispatch({type:LOGIN_SUCCESS, data: data})
+					}else{
+						dispatch({type:LOGIN_INCORRECT, data: data})
+					}
+				}
+				error => dispatch({type:LOGIN_ERROR})
+				//maybe dispatch an action that does some sort of notification on screen?
+			);
+	}
+}
+
+export const UPDATE_FIELD = 'UPDATE_FIELD';
+
+export const updateFieldAction = (key, text) => {
+	return (dispatch)=>{
+		dispatch({
+			type:UPDATE_FIELD,
+			text: text,
+			key: key
+		})
+	}
+}
+
--- ttr/src/reducers/users.reducer.js
+++ ttr/src/reducers/users.reducer.js
@@ -1,14 +1,31 @@
 import * as _ from '../actions/users.actions';
 
-const users = (state = {}, action) => {
+const users = (state = {fields:{}}, action) => {
   switch (action.type) {
+
+    //Section for creating user
   	case _.CREATE_USER_PENDING:
-  		return {...state, pending: true};
+  		return {...state, createUserPending: true};
   	case _.CREATE_USER_SUCCESS:
-  		return {...state, userId: action.data._id, 
-  				userGames: action.data.games, pending: false};
+  		return {...state, createUserId: action.data._id, 
+  				userGames: action.data.games, createUserPending: false};
   	case _.CREATE_USER_ERROR:
-  		return {...state, errorMsg: action.data.error, pending: false};
+  		return {...state, createUserErrorMsg: 'Error creating user.', createUserPending: false};
+
+    //Section for logging in
+    case _.LOGIN_PENDING:
+    case _.LOGIN_SUCCESS:
+    case _.LOGIN_ERROR:
+    case _.LOGIN_INCORRECT:
+      return {...state, login:'yeah!'};
+
+
+    //Section for user login form
+    case _.UPDATE_FIELD:
+      var temp = state.fields;
+      temp[action.key] = action.text
+      return {...state, fields: temp}
+
     default:
       	return state;
   }

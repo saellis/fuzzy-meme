b23afbfd16d63cbf9b3c3a3d7aa632cf66f3b704
jaswinne <jaswinne@uark.edu>
2017-11-21 13:50:15 -0600

    towards 100% code coverage on login component test coverage

--- ttr/src/components/login/__specs__/login.component.spec.js
+++ ttr/src/components/login/__specs__/login.component.spec.js
@@ -2,20 +2,47 @@ import Login from '../login.component.jsx';
 import LoginFieldContainer from '../../../containers/login/loginField.container.jsx';
 
 import React from 'react';
+import { Button } from 'react-dom'
 import { mount, shallow } from 'enzyme';
 import { loginAction }  from '../../../actions/users.actions';
 
 
 describe('<Login>', function () {
+	var wrapper;
+	beforeEach(() => {
+		wrapper = shallow(<Login.Login />);
+	})
+	it('should have two inputs', function () {
+		expect(wrapper.find(LoginFieldContainer)).to.have.length(2);
+	});
 
-  it('should have two inputs', function () {
-    const wrapper = shallow(<Login/>);
-    expect(wrapper.find(LoginFieldContainer)).to.have.length(2);
-  });
+	it('first input should be for username', () => {
+		const field = wrapper.find(LoginFieldContainer).at(0);
+		expect(field.props().id).to.match(/username$/i);
+		expect(field.props().type).to.match(/username$/i);
+		expect(field.props().placeholder).to.match(/username$/i);
+		expect(field.props().textChange).to.be.a('function');
+	});
 
-  it('should have a button', function () {
-    const wrapper = shallow(<Login/>);
-    expect(wrapper.find('button')).to.have.length(1);
-  });
+	it('second input should be for password', () => {
+		const field = wrapper.find(LoginFieldContainer).at(1);
+		expect(field.props().id).to.match(/password$/i);
+		expect(field.props().type).to.match(/password$/i);
+		expect(field.props().placeholder).to.match(/password$/i);
+		expect(field.props().textChange).to.be.a('function');
+	});
+
+	it('should have a button', function () {
+		const field = wrapper.find('button');
+		expect(field).to.have.length(1);
+		expect(true).to.equal(false);
+	});
+
+	it('should save on keypress', () => {
+		wrapper.find(LoginFieldContainer).forEach((field) => {
+			field.props().textChange(field.props().id, 'test');
+			expect(Login.getFields()[field.props().id]).to.equal('test');
+		})
+	});
 
 });

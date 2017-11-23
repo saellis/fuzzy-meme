c16335f12c0ee585ee0946c47f646e01f18a96eb
jaswinne <jaswinne@uark.edu>
2017-11-23 14:17:14 -0600

    Panelized login

--- client/src/components/login/login.component.jsx
+++ client/src/components/login/login.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import LoginFieldContainer from '../../containers/login/loginField.container.jsx'
+import { LoginFieldContainer } from '../../containers/login/loginField.container.jsx'
 
-import { Button } from 'react-bootstrap';
+import { Button, Col, Panel } from 'react-bootstrap';
 
 const fields = {};
 
@@ -17,14 +17,16 @@ const Login = {
 	Login: (props) => {
 
 		return(
-			<div>
-				<span>{props.syntaxErrorText}</span>
-				<LoginFieldContainer id='loginUsername' type='loginUsername' label='Username: '
-					placeholder='Username' textChange={(key,value) => Login.textChange(key,value)}/>
-				<LoginFieldContainer id='loginPassword' type='loginPassword' label='Password: '
-					placeholder='Password' textChange={(key,value) => Login.textChange(key,value)}/>
-				<Button block className='btn-primary' onClick={()=> props.login(fields['loginUsername'], fields['loginPassword'])} >LOGIN</Button>
-			</div>
+			<Col xs={10} sm={10}  md={6} lg={6} xsOffset={1} smOffset={1} mdOffset={3} lgOffset={3} >
+				<Panel bsStyle='primary'>
+					<span>{props.syntaxErrorText}</span>
+					<LoginFieldContainer id='loginUsername' type='loginUsername' label='Username: '
+						placeholder='Username' textChange={(key,value) => Login.textChange(key,value)}/>
+					<LoginFieldContainer id='loginPassword' type='loginPassword' label='Password: '
+						placeholder='Password' textChange={(key,value) => Login.textChange(key,value)}/>
+					<Button block className='btn-primary' onClick={()=> props.login(fields['loginUsername'], fields['loginPassword'])} >LOGIN</Button>
+				</Panel>
+			</Col>
 		)}
 }
 

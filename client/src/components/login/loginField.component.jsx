4b2488af171ed46d7bd17bd71cdd26631a917417
jaswinne <jaswinne@uark.edu>
2017-11-23 14:17:02 -0600

    Login Field Updates

--- client/src/components/login/loginField.component.jsx
+++ client/src/components/login/loginField.component.jsx
@@ -2,21 +2,40 @@ import React from 'react';
 
 import { FormControl, FormGroup, ControlLabel , Col} from 'react-bootstrap';
 
-const LoginField =  (props) => {
-	return (
-		<div>
-			<FormGroup >
-				<Col xs={4} sm={4}  md={4} lg={4} className='margin-bottom-5'>
-					<ControlLabel>{props.label}</ControlLabel>
+export default class LoginField extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {value: ''};
+	}
+
+	parseType() {
+		return this.props.type.toLowerCase().indexOf('password') !== -1 ? 'password' : 'text';
+	}
+
+	handleChange(event) {
+		this.props.textChange(this.props.type, event.target.value);
+		this.setState({value: event.target.value});
+	}
+
+	getValidationState() {
+		if(!this.props.regex){
+			return null;
+		}
+		return this.state.value.match(this.props.regex) ? 'success' : (this.state.value.length > 0 ? 'error' : null);
+	}
+
+	render() {
+		return(
+			<FormGroup validationState={this.getValidationState()}>
+				<Col xs={2} sm={2}  md={4} lg={4} className='margin-bottom-5'>
+					<ControlLabel>{this.props.label}</ControlLabel>
 				</Col>
 				<Col xs={10} sm={10}  md={8} lg={8}  className='margin-bottom-5' >
-					<FormControl type={props.type.toLowerCase().indexOf('password') !== -1 ? 'password' : 'text'}
-						placeholder={props.placeholder}
-						onChange={(evt) => props.textChange(props.type, evt.target.value)} />
+					<FormControl type={this.parseType()}
+						placeholder={this.props.placeholder}
+						onChange={(evt) => this.handleChange(evt)} />
 				</Col>
 			</FormGroup>
-		</div>
-	)}
-
-
-export default LoginField;
+		);
+	}
+};
--- client/src/containers/login/loginField.container.jsx
+++ client/src/containers/login/loginField.container.jsx
@@ -2,7 +2,4 @@ import { connect } from 'react-redux'
 import user from '../../components/login/loginField.component.jsx';
 
 
-const LoginFieldContainer = connect()(user)
-
-
-export default LoginFieldContainer;
+export const LoginFieldContainer = connect()(user)

97605bee365e52fffaea86b0b8e3d6bfec3f3a7d
jaswinne <jaswinne@uark.edu>
2017-11-25 18:14:32 -0600

    added react-router

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import 'react-tabs/style/react-tabs.css';
 import './libs/fa/css/font-awesome.min.css';
 
-import Root from './root.component';
+import UserPortalContainer from './containers/login/userPortal.container.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
 import { Provider } from 'react-redux';
@@ -14,16 +14,33 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
+
+import createHistory from 'history/createBrowserHistory'
+import { Route } from 'react-router'
+
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
+
 import baseReducer from './reducers/base.reducer';
 
+// Create a history of your choosing (we're using a browser history in this case)
+const history = createHistory()
 
-let store = createStore(
-	baseReducer,
-	applyMiddleware(thunk, logger));
+// Build the middleware for intercepting and dispatching navigation actions
+const middleware = routerMiddleware(history)
+
+// Add the reducer to your store on the `router` key
+// Also apply our middleware for navigating
+const store = createStore(
+  baseReducer,
+	applyMiddleware(thunk, middleware, logger));
 
 ReactDOM.render(
   <Provider store={store}>
-    <Root />
+		<ConnectedRouter history={history}>
+		 <div className='text-center'>
+			 <Route exact path="/" component={UserPortalContainer}/>
+		 </div>
+	 </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
--- client/src/reducers/base.reducer.js
+++ client/src/reducers/base.reducer.js
@@ -2,9 +2,12 @@ import { combineReducers } from 'redux';
 
 import users from './login/users.base.reducer';
 import games from './games.reducer';
+import {routerReducer} from 'react-router-redux'
+
 
 export default combineReducers({
 	games: games,
-	users: users
+	users: users,
+	router: routerReducer
 
 });

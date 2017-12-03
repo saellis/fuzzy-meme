392417f48efd48a06c322406eda5d39fce2ec957
jaswinne <jaswinne@uark.edu>
2017-12-03 14:27:03 -0600

    async-ed the actions

--- client/src/actions/login/createUser.actions.js
+++ client/src/actions/login/createUser.actions.js
@@ -1,30 +1,31 @@
 import * as _ from '../../constants/login/createUser.actions.constants';
 
+import to from 'await-to-js';
+
 export const createUserAction = (un, pw) => {
-	return (dispatch)=>{
+	return async (dispatch)=>{
 		dispatch({type:_.CREATE_USER_PENDING});
 		dispatch({type:_.CLEAR_CREATE_USER_ERROR});
 		dispatch({type:_.CLEAR_CREATE_USER_SUCCESS});
-		return fetch('/users/create', {
+		let [err, res] = await to(fetch('/users/create', {
 			method: 'post',
 			headers: {
 				'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
 			},
 			body: `username=${un}&password=${pw}`
-		}).then(res => res.json()).then(
-				data => {
-					if(data.err){
-						dispatch({type:_.CREATE_USER_ERROR, error: data.err});
-					}else{
-						dispatch({type:_.CREATE_USER_SUCCESS, data: data});
-						dispatch({type:_.RESET_CREATE_FORM});
-						dispatch({type:_.CLEAR_CREATE_USER_SYNTAX_ERROR});
-					}
-
-				},
-				error => dispatch({type:_.CREATE_USER_ERROR})
-				//maybe dispatch an action that does some sort of notification on screen?
-			);
+		}))
+		if(err){
+			dispatch({type:_.CREATE_USER_ERROR})
+		}else{
+			let data = await res.json();
+			if(data.err){
+				dispatch({type:_.CREATE_USER_ERROR, error: data.err});
+			}else{
+				dispatch({type:_.CREATE_USER_SUCCESS, data: data});
+				dispatch({type:_.RESET_CREATE_FORM});
+				dispatch({type:_.CLEAR_CREATE_USER_SYNTAX_ERROR});
+			}
+		}
 	};
 };
 
--- client/src/actions/menu/games/menu.games.actions.js
+++ client/src/actions/menu/games/menu.games.actions.js
@@ -1,4 +1,5 @@
 import * as _ from '../../../constants/menu/games/menu.games.actions.constants';
+import to from 'await-to-js';
 
 export const createGameAction = (uid) => {
 	return (dispatch)=>{
@@ -27,20 +28,19 @@ export const createGameAction = (uid) => {
 
 
 export const loadGamesAction = (uid) => {
-	return (dispatch)=>{
+	return async (dispatch)=>{
 		dispatch({type:_.LOAD_GAMES_PENDING});
-		return fetch(`/games?userId=${uid}`, {
+		let [err,res] = await to(fetch(`/games?userId=${uid}`, {
 			method: 'get',
 			headers: {
 				'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
 			}
-		}).then(res => res.json()).then(
-				data => {
-        	dispatch({type:_.LOAD_GAMES_SUCCESS, data: data});
-				},
-				error => {
-        	dispatch({type:_.LOAD_GAMES_ERROR});
-				}//maybe dispatch an action that does some sort of notification on screen?
-			);
+		}))
+		if(err){
+			dispatch({type:_.LOAD_GAMES_ERROR});
+		}else{
+			let data = await res.json();
+			dispatch({type:_.LOAD_GAMES_SUCCESS, data: data});
+		}
 	};
 };

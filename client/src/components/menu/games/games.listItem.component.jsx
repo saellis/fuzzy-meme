14e2f30a63fb1bd82985245b39669f49e4660bb3
jaswinne <jaswinne@uark.edu>
2017-12-05 08:29:10 -0600

    added template boilerplate to list items

--- client/src/components/menu/games/games.listItem.component.jsx
+++ client/src/components/menu/games/games.listItem.component.jsx
@@ -18,12 +18,19 @@ export class GamesListItem extends React.Component{
 	render(){
 
 		return(
-      <div
-        className="list-group-item"
-        onClick={() => {alert('asd')}}
+      <ListGroupItem
+				bsStyle="info"
+        className="list-group-item list-group-item-action flex-column align-items-start"
+        onClick={() => {}}
       >
-        {this.props.children}
-      </div>
+
+				<div className="d-flex w-100 justify-content-between">
+      <span className="h5 mb-1">{this.props.children}</span>
+      <small className='pull-right'>3 days ago</small>
+    </div>
+    <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+    <small>Donec id elit non mi porta.</small>
+      </ListGroupItem>
 		)}
 }
 
